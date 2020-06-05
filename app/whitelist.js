7e8bd43f57d4d2b59077611f1cee63b184e37e29
Strain <strain.mattl@gmail.com>
2020-06-05 10:44:59 -0400

    Updates for Fixed Basic Code

--- app/whitelist.js
+++ app/whitelist.js
@@ -6,6 +6,7 @@ function preload(){
   whitelist = loadStrings('app/applist.txt');
 }
 
+function setup() {
 var substringMatcher = function(strs) {
     return function findMatches(q, cb) {
       var matches, substringRegex;
@@ -43,10 +44,54 @@ var substringMatcher = function(strs) {
           'Unable to find any application on the whitelist that match the current query. You may submit a whitelist request via <a href="mailto:CISTAC@caci.com">CISTAC@caci.com</a>.',
         '</div>',
       ].join('\n')
-    }
+    },
   });
 
 
-  function setup() {
-    console.log(whitelist);
+    //attempting to simply allow for toggle of the boxes to different underlying whitelist TXT files.
+    var button1 = document.getElementById('CACI');
+    button1.addEventListener("click", ListChanger('CACI'));
+    
+    var button2 = document.getElementById('UDEV');
+    button2.addEventListener("click", ListChanger('UDEV'));
+    
+    var button3 = document.getElementById('LGSDirect');
+    button3.addEventListener("click", ListChanger('LGSDirect'));
+    
+    var button4 = document.getElementById('Critical Insight');
+    button4.addEventListener("click", ListChanger('Critical Insight'));
+    
+    function ListChanger(listname) {
+      if (listname = 'CACI') {
+          whitelist = loadStrings('app/applist.txt');
+          console.log(whitelist);
+      } else if (listname = 'UDEV') {
+        whitelist = loadStrings('app/udev-applist.txt');
+        console.log(whitelist);
+      } else if (listname = 'LGSDirect') {
+        whitelist = loadStrings('app/lgsdirect-applist.txt');
+        console.log(whitelist);
+      } else if (listname = 'Critical Insight') {
+        whitelist = loadStrings('app/criticalinsight-applist.txt');
+        console.log(whitelist);
+     }
+    }
+
+
+//attempting to bind or align on-event listeners to the click against a found item.
+$('.typehead').bind('typeahead:select', function(ev, suggestion) {
+  document.getElementById('result').innerHTML="The item you have chosen exists on the CACI Whitelist and may be able to be installed.";
+  console.log('Selection: ' + suggestion);
+});
+
+var NotifyUser = function() {
+  document.getElementById('result').innerHTML="The item you have chosen exists on the CACI Whitelist and may be able to be installed.";
+}; 
+
+var elements = document.getElementsByClassName("tt-suggestion tt-selectable");
+
+for (var i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('click', NotifyUser, false);
+};
+
 };
