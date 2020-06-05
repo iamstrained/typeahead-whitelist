3ff1d792074f6df718b8fe20f8178899b79f752b
Strain <strain.mattl@gmail.com>
2020-06-05 16:44:17 -0400

    Update2

--- app/whitelist.js
+++ app/whitelist.js
@@ -1,7 +1,5 @@
-var fileURL = '/app/applist.txt';
 var whitelist;
 
-//var fs = require('fs')
 function preload(){
   whitelist = loadStrings('app/applist.txt');
 }
@@ -49,29 +47,33 @@ var substringMatcher = function(strs) {
 
     //attempting to simply allow for toggle of the boxes to different underlying whitelist TXT files.
     //doesn't appear to be successfully tying these listeners to the buttons and it's not firing actively. Perhaps this is due to being in $SETUP versus being somewhere else - i'm not clear on when in the DOM ordering it should exist.
-    var button1 = document.getElementById("CACI");
-    button1.addEventListener("click", ListChanger("CACI"));
+  var button1 = document.getElementById('CACI');
+    button1.addEventListener("click", function() {ListChanger('CACI')});
     
-    var button2 = document.getElementById("UDEV");
-    button2.addEventListener("click", ListChanger("UDEV"));
+  var button2 = document.getElementById('UDEV');
+    button2.addEventListener("click", function() {ListChanger('UDEV')});
     
-    var button3 = document.getElementById("LGSDirect");
-    button3.addEventListener("click", ListChanger("LGSDirect"));
+  var button3 = document.getElementById('LGSDirect');
+    button3.addEventListener("click", function() {ListChanger('LGSDirect')});
     
-    var button4 = document.getElementById("Critical Insight");
-    button4.addEventListener("click", ListChanger("Critical Insight"));
-    
+   var button4 = document.getElementById('Critical Insight');
+    button4.addEventListener("click", function() {ListChanger('Critical Insight')});
+
     function ListChanger(listname) {
       if (listname = 'CACI') {
-          whitelist = loadStrings('app/applist.txt');
+        whitelist = undefined  
+        whitelist = loadStrings('app/applist.txt');
           console.log(whitelist);
       } else if (listname = 'UDEV') {
+        whitelist = undefined
         whitelist = loadStrings('app/udev-applist.txt');
         console.log(whitelist);
       } else if (listname = 'LGSDirect') {
+        whitelist = undefined
         whitelist = loadStrings('app/lgsdirect-applist.txt');
         console.log(whitelist);
       } else if (listname = 'Critical Insight') {
+        whitelist = undefined
         whitelist = loadStrings('app/criticalinsight-applist.txt');
         console.log(whitelist);
      }
@@ -79,12 +81,12 @@ var substringMatcher = function(strs) {
 
 //attempting to bind or align on-event listeners to the click against a found item. This is using the typeahead:select portion of the library.
 // but it doesn't seem to fire the way i would expect when i click a suggestion item. It 'should' create a note for the user that tells them 'A-OK'.
-  $('.typehead').bind('typeahead:select', function(ev, suggestion) {
-    document.getElementById('result').innerHTML="The item you have chosen exists on the CACI Whitelist and may be able to be installed.";
+  $('.typeahead').bind('typeahead:select', function(ev, suggestion) {
+    document.getElementById('result').innerHTML="Great news, the item you have chosen, " + suggestion + ", exists on the whitelist and may be able to be installed. ";
     console.log('Selection: ' + suggestion);
   });
 
-  var NotifyUser = function() {
+/*   var NotifyUser = function() {
     document.getElementById('result').innerHTML="The item you have chosen exists on the CACI Whitelist and may be able to be installed.";
   }; 
 
@@ -93,5 +95,5 @@ var substringMatcher = function(strs) {
   for (var i = 0; i < elements.length; i++) {
       elements[i].addEventListener('click', NotifyUser, false);
   };
-
+ */
 };
