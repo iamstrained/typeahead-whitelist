f7e16034c5db87969ac8c39864d47534e21a2ead
Strain <strain.mattl@gmail.com>
2020-06-05 18:21:47 -0400

    Update3

--- app/whitelist.js
+++ app/whitelist.js
@@ -27,57 +27,71 @@ var substringMatcher = function(strs) {
     };
   };
 
- $('#whitelist-search .typeahead').typeahead({
-    hint: true,
-    highlight: true,
-    minLength: 3,
-    limit: 10
-  },
-  {
-    name: 'whitelistapps',
-    source: substringMatcher(whitelist),
-    templates: {
-      empty: [
-        '<div class="empty-message">',
-          'Unable to find any application on the whitelist that match the current query. You may submit a whitelist request via <a href="mailto:CISTAC@caci.com">CISTAC@caci.com</a>.',
-        '</div>',
-      ].join('\n')
-    },
-  });
 
-    //attempting to simply allow for toggle of the boxes to different underlying whitelist TXT files.
-    //doesn't appear to be successfully tying these listeners to the buttons and it's not firing actively. Perhaps this is due to being in $SETUP versus being somewhere else - i'm not clear on when in the DOM ordering it should exist.
+
+//this section feeds the buttons to the function for assigning the unique whitelist file to a variable array. 
   var button1 = document.getElementById('CACI');
-    button1.addEventListener("click", function() {ListChanger('CACI')});
+    button1.addEventListener("click", function() {
+      ListChanger('CACI');
+    }
+    );
     
   var button2 = document.getElementById('UDEV');
-    button2.addEventListener("click", function() {ListChanger('UDEV')});
+    button2.addEventListener("click", function() {
+      ListChanger('UDEV');
+    }
+    );
     
   var button3 = document.getElementById('LGSDirect');
-    button3.addEventListener("click", function() {ListChanger('LGSDirect')});
+    button3.addEventListener("click", function() {
+      ListChanger('LGSDirect');
+    }
+    );
     
    var button4 = document.getElementById('Critical Insight');
-    button4.addEventListener("click", function() {ListChanger('Critical Insight')});
+    button4.addEventListener("click", function() {
+      ListChanger('Critical Insight');
+    }
+    );
 
     function ListChanger(listname) {
-      if (listname = 'CACI') {
+      if (listname == 'CACI') {
         whitelist = undefined  
         whitelist = loadStrings('app/applist.txt');
           console.log(whitelist);
-      } else if (listname = 'UDEV') {
+      } else if (listname == 'UDEV') {
         whitelist = undefined
         whitelist = loadStrings('app/udev-applist.txt');
         console.log(whitelist);
-      } else if (listname = 'LGSDirect') {
+      } else if (listname == 'LGSDirect') {
         whitelist = undefined
         whitelist = loadStrings('app/lgsdirect-applist.txt');
         console.log(whitelist);
-      } else if (listname = 'Critical Insight') {
+      } else if (listname == 'Critical Insight') {
         whitelist = undefined
         whitelist = loadStrings('app/criticalinsight-applist.txt');
         console.log(whitelist);
      }
     }
+
+    $('#whitelist-search .typeahead').typeahead({
+      hint: true,
+      highlight: true,
+      minLength: 3,
+      limit: 10
+    },
+    {
+      name: 'whitelistapps',
+      source: substringMatcher(whitelist),
+      templates: {
+        empty: [
+          '<div class="empty-message">',
+            'Unable to find any application on the whitelist that match the current query. You may submit a whitelist request via <a href="mailto:CISTAC@caci.com">CISTAC@caci.com</a>.',
+          '</div>',
+        ].join('\n')
+      },
+    });
+
 
 //attempting to bind or align on-event listeners to the click against a found item. This is using the typeahead:select portion of the library.
 // but it doesn't seem to fire the way i would expect when i click a suggestion item. It 'should' create a note for the user that tells them 'A-OK'.
